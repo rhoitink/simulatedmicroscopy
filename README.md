aafdb8e4d1788274c3e5184221c7cfe1b632a8a1
Roy Hoitink <L.D.Hoitink@uu.nl>
2022-11-24 16:39:33 +0100

    Package structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simulated Microscopy
+
+Python code to simulate 3D confocal/STED microscopy data based on particle coordinates.
+
+Written by Roy Hoitink (L.D.Hoitink@uu.nl)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simulatedmicroscopy",
+    version="0.1.0",
+    author="Roy Hoitink",
+    author_email="L.D.Hoitink@uu.nl",
+    long_description=open("README.md").read(),
+    packages=find_packages(include=["simulatedmicroscopy", "simulatedmicroscopy.*"]),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "h5py",
+        "scipy",
+    ],
+    extras_require={
+        "dev": [
+            "black",
+            "flake8",
+        ]
+    },
+)
