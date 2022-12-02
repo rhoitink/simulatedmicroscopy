edc640294c33ba4fb5a9dce4840131240808e54e
Roy Hoitink <L.D.Hoitink@uu.nl>
2022-12-02 09:34:38 +0100

    Extra checks in coordinates input

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -22,6 +22,50 @@ def test_coordinates():
     assert (cs.coordinates == coordinates).all()
 
 
+def test_coordinates_2D():
+    coordinates = np.array([[1.0, 0.0], [0.0, 1.0], [2, 0.0], [0.0, 2.0]])
+
+    cs = Coordinates(coordinates)
+
+    assert (cs.coordinates == coordinates).all()
+
+
+def test_coordinates_wrongshape():
+    # instead of given an (N,3) array, give (3,N) array
+    coordinates = demo_coordinates().T
+
+    # should atomatically transpose but also give a warning
+    with pytest.warns(Warning):
+        cs = Coordinates(coordinates)
+
+    assert cs.coordinates.shape[0] == coordinates.shape[1]
+
+
+def test_coordinates_wrongshape_1d():
+    # instead of given an (N,3) array, give 1d array)
+    coordinates = demo_coordinates().flat
+
+    # should raise ValueError
+    with pytest.raises(ValueError):
+        Coordinates(coordinates)
+
+def test_coordinates_wrongshape_1d():
+    # instead of given an (N,3) array, give 1d array)
+    coordinates = demo_coordinates().flat
+
+    # should raise ValueError
+    with pytest.raises(ValueError):
+        Coordinates(coordinates)
+
+def test_coordinates_wrongshape_5x5():
+    # instead of given an (N,3) array, give 5x5 array)
+    coordinates = np.zeros(shape=(5,5))
+
+    # should raise ValueError
+    with pytest.raises(ValueError):
+        Coordinates(coordinates)
+
+
 @pytest.mark.parametrize("scaling_factor", [0.3, 1.0, 1.5, 2.0])
 def test_scaling(scaling_factor):
     cs = Coordinates(demo_coordinates())
@@ -30,3 +74,11 @@ def test_scaling(scaling_factor):
 
     assert (cs.coordinates == scaled_coords).all()
     assert (cs.coordinates == demo_coordinates() * scaling_factor).all()
+
+@pytest.mark.parametrize("unit,scaling_factor", [("nm", 1e3), ("µm", 1.)])
+def test_get_coordinates_unit(unit, scaling_factor):
+    cs = Coordinates(demo_coordinates())
+
+    converted_coords = cs.get_coordinates(unit=unit)
+
+    assert (converted_coords == demo_coordinates() * scaling_factor).all()
